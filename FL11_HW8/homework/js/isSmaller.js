d42352d2f045131d1e68b157527526a5bcbe7b57
Oleksandr Matiash <alex.matiash97@gmail.com>
2019-07-13 11:38:13 +0300

    fix isSmaller HW8

--- FL11_HW8/homework/js/isSmaller.js
+++ FL11_HW8/homework/js/isSmaller.js
@@ -1,2 +1,2 @@
-const isSmaller = (FirstNumber, SecondNumber) => FirstNumber < SecondNumber;
-console.log(isSmaller(2,5));
+const isBigger = (FirstNumber, SecondNumber) => FirstNumber < SecondNumber;
+console.log(isBigger(2,5));
