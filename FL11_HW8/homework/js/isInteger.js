ee021e04f43a104bb2b3f7a279632812576d4baf
Oleksandr Matiash <alex.matiash97@gmail.com>
2019-07-13 11:43:28 +0300

    fix isInteger HW8

--- FL11_HW8/homework/js/isInteger.js
+++ FL11_HW8/homework/js/isInteger.js
@@ -1,3 +1,3 @@
-const isInteger = (NumberForCheck) => Number.isInteger(NumberForCheck);
+const isInteger = (NumberForCheck) => Math.floor(NumberForCheck) === NumberForCheck;
 console.log(isInteger(4));
 console.log(isInteger(4.5));
