cd0e0dbb1bbbe891151bfdd8dd3c757473a97f92
Oleksandr Matiash <alex.matiash97@gmail.com>
2019-07-16 20:49:41 +0300

    final Task 8 HW9

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -101,3 +101,54 @@ function daysBetween(dataStart, dataEnd) {
 }
 
 daysBetween(new Date('2016-03-18T00:00:00'), new Date('2016-04-19T00:00:00'));
+
+// Task 8
+let data = [
+    {
+        "_id": "5b5e3168c6bf40f2c1235cd6",
+        "index": 0,
+        " birthday ": '2016-03-18T00:00:00',
+        "eyeColor": "green",
+        "name": "Stein",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e3168e328c0d72e4f27d8",
+        "index": 1,
+        " birthday ": '1991-02-11T00:00:00',
+        "eyeColor": "blue",
+        "name": "Cortez",
+        "favoriteFruit": "strawberry"
+    },
+    {
+        "_id": "5b5e3168cc79132b631c666a",
+        "index": 2,
+        " birthday ": '1984-04-17T00:00:00',
+        "eyeColor": "blue",
+        "name": "Suzette",
+        "favoriteFruit": "apple"
+    },
+    {
+        "_id": "5b5e31682093adcc6cd0dde5",
+        "index": 3,
+        " birthday ": '1994-04-17T00:00:00',
+        "eyeColor": "green",
+        "name": "George",
+        "favoriteFruit": "banana"
+    }
+];
+
+function getAmountOfAdultPeople(dataGetAmountOfAdultPeople) {
+    const dateToday = new Date();
+    let peopleCount = 0,
+        i;
+    for (i = 0; i < dataGetAmountOfAdultPeople.length; i++) {
+        let ageToday = daysBetween(dataGetAmountOfAdultPeople[i][" birthday "], dateToday) / 365;
+        if (ageToday > 18) {
+            peopleCount += 1;
+        }
+    }
+    return peopleCount;
+}
+
+getAmountOfAdultPeople(data);
