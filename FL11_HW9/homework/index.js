c7f95b51017614297d9433966f83e0c4f0103314
Oleksandr Matiash <alex.matiash97@gmail.com>
2019-07-16 19:25:03 +0300

    final Task 1 HW9

--- FL11_HW9/homework/index.js
+++ FL11_HW9/homework/index.js
@@ -13,3 +13,21 @@ function getNumbers(stringFromUser) {
 getNumbers('string');
 getNumbers('n1um3ber95');
 
+// Task 1
+function findTypes() {
+    let result = {},
+        i;
+
+    for (i = 0; i < arguments.length; i++) {
+        if (typeof arguments[i] in result) {
+            result[typeof arguments[i]] += 1;
+        } else {
+            result[typeof arguments[i]] = 1;
+        }
+    }
+
+    return result;
+}
+
+findTypes(`number`);
+findTypes(null, 5, `hello`);
